fabb5ae54b246730a3c85fe382ab0a613db26a6d
Kitty <77138477+kamork@users.noreply.github.com>
2023-03-27 12:57:47 -0700

    💯

---
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    var tip = 0.10
+    var numberOfPeople = 2
+    var billTotal = 0.0
+    var finalResult = "0.0"
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+        // Deselect all tip buttons
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        // Highlight selected tip button
+        sender.isSelected = true
+        
+        // Convert selected tip to decimal
+        tip = (Double(String(sender.currentTitle!).dropLast())!) / 100
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        numberOfPeople = Int(sender.value)
+        splitNumberLabel.text = String(numberOfPeople)
+    }
+    
+    @IBAction func calculatePressed(_ sender: Any) {
+        let bill = billTextField.text!
+        if bill != "" {
+            billTotal = Double(bill)!
+            let result = billTotal * (1 + tip) / Double(numberOfPeople)
+            finalResult = String(format: "%.2f", result)
+        }
+        
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultViewController
+            
+            destinationVC.result = finalResult
+            destinationVC.tip = Int(tip * 100)
+            destinationVC.split = numberOfPeople
+        }
+    }
+    
+}
+
